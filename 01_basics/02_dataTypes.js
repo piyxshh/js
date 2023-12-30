27324248b529ae58d71ef62ba8d0be2d060e9721
piyush <133100406+piyxshh@users.noreply.github.com>
2023-12-30 19:45:35 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser so syntax will be different
+
+// datatypes:
+// number => 2 to the power 53 tak
+// bigint => for huge numbers 
+// string => ""
+// boolean => true/false value 
+// null => stand alone value 
+// undefined => when value is not defined
+// symbol => unique
+
+// object 
+
+let name = "piyush"   // string
+let age = 18          // number
+let isLoggedIn = false    // boolean
+let state = null   // null value is assigned
+// let state; // now state has not been assigned any value hence it is undefined
+
+console.log(typeof "piyush"); // this will give you the output as string
+console.log(typeof null); // this will give u object
+console.log(typeof undefined); // this will give undefined as the type
