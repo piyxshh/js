b0407208b62bba20fc7f1054cffbe0f35a971203
piyush <133100406+piyxshh@users.noreply.github.com>
2024-01-20 07:53:10 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,19 @@
+// syntax of a function
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()    // by adding the parenthesis after the function name it will call out the function and execute the value in itself
+                  // wihtout parenthesis it wont return any value
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}                
+
+// addTwoNumbers()  //this will not execute anything as theres no arguement given
+   addTwoNumbers(3, 4)   //this will add these two args now
