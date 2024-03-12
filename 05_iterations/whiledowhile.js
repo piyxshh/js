81702fd736139dbdf6bd33cf0cab7af161328ecf
piyush <133100406+piyxshh@users.noreply.github.com>
2024-03-12 13:29:29 +0000

    loops

---
+++ 05_iterations/whiledowhile.js
@@ -0,0 +1,46 @@
+
+//while loop
+
+
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+
+// while loop with array
+
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+
+//do while loop
+
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+
+
+// let score = 11                                // in do while loop first do command is run and then when condition is checked
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
