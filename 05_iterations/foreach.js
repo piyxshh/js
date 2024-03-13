90b1419c7a94146fcb61d2b5a4f5bfb878281b9a
piyush <133100406+piyxshh@users.noreply.github.com>
2024-03-13 07:20:54 +0000

    for each

---
+++ 05_iterations/foreach.js
@@ -0,0 +1,48 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){                           //for each can call back a function without having to name the function
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {                               // using arrow function
+    // console.log(item);
+// } )
+
+
+
+
+// function printMe(item){                                     
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+
+
+
+
+
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+// myCoding.forEach( (item) => {
+    
+//     console.log(item.languageName);
+// } )
